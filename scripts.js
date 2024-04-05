e42be135f438c67173b755641eb41d061fc63ca6
malena-3 <137833097+malena-3@users.noreply.github.com>
2024-04-05 00:15:51 -0700

    first commit. Figuring out the layout and slowly editing.

--- scripts.js
+++ scripts.js
@@ -24,34 +24,37 @@
  */
 
 
-const FRESH_PRINCE_URL = "https://upload.wikimedia.org/wikipedia/en/3/33/Fresh_Prince_S1_DVD.jpg";
+const PILLER_URL = "https://png.pngtree.com/png-clipart/20220620/original/pngtree-green-bug-cartoon-cute-caterpillar-clipart-png-image_8137016.png";
 const CURB_POSTER_URL = "https://m.media-amazon.com/images/M/MV5BZDY1ZGM4OGItMWMyNS00MDAyLWE2Y2MtZTFhMTU0MGI5ZDFlXkEyXkFqcGdeQXVyMDc5ODIzMw@@._V1_FMjpg_UX1000_.jpg";
 const EAST_LOS_HIGH_POSTER_URL = "https://static.wikia.nocookie.net/hulu/images/6/64/East_Los_High.jpg";
 
-// This is an array of strings (TV show titles)
-let titles = [
+// This is an array of strings (bug names)
+let bugNames = [
     "Fresh Prince of Bel Air",
     "Curb Your Enthusiasm",
-    "East Los High"
+    "East Los High",
+    "Hello this is a test"
 ];
+
+
 // Your final submission should have much more data than this, and 
 // you should use more than just an array of strings to store it all.
 
 
-// This function adds cards the page to display the data in the array
+// This function adds bug stickers the page to display the data in the array
 function showCards() {
     const cardContainer = document.getElementById("card-container");
     cardContainer.innerHTML = "";
     const templateCard = document.querySelector(".card");
     
-    for (let i = 0; i < titles.length; i++) {
-        let title = titles[i];
+    for (let i = 0; i < bugNames.length; i++) {
+        let title = bugNames[i];
 
         // This part of the code doesn't scale very well! After you add your
         // own data, you'll need to do something totally different here.
         let imageURL = "";
         if (i == 0) {
-            imageURL = FRESH_PRINCE_URL;
+            imageURL = PILLER_URL;
         } else if (i == 1) {
             imageURL = CURB_POSTER_URL;
         } else if (i == 2) {
